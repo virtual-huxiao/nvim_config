b8d9d08ec44a6c82a8af3a7fd7c4c220a951d891
胡箫儿 <345609501@qq.com>
2022-12-04 17:18:33 +0800

    modify s to double char end , S to double char begin

--- lua/leap_.lua
+++ lua/leap_.lua
@@ -1,11 +1,12 @@
 -- require('leap').add_default_mappings()
 
 -- full window search jump
-vim.keymap.set({'n','v'}, 's', function () require('leap').leap { target_windows = { vim.fn.win_getid() } } end)
+vim.keymap.set({'n','v'}, 's', function () require('leap').leap { offset = 2, target_windows = { vim.fn.win_getid() } } end)
+vim.keymap.set({'n','v'}, 'S', function () require('leap').leap { offset = 0, target_windows = { vim.fn.win_getid() } } end)
 
 -- all windows jump
-vim.keymap.set('n', 'S',
-function () require('leap').leap { target_windows = vim.tbl_filter(
-  function (win) return vim.api.nvim_win_get_config(win).focusable end,
-  vim.api.nvim_tabpage_list_wins(0)
-)} end )
+-- vim.keymap.set('n', 'S',
+-- function () require('leap').leap { target_windows = vim.tbl_filter(
+--  function (win) return vim.api.nvim_win_get_config(win).focusable end,
+--  vim.api.nvim_tabpage_list_wins(0)
+--)} end )
