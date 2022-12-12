ef6d523c28071e1b00f0d7ce8d542f1badb5f7a6
shi.fengyuan <shi.fengyuan@xitang-ai.com>
2022-12-12 16:58:05 +0800

    chang dashboard action

--- lua/dashboard-nvim_.lua
+++ lua/dashboard-nvim_.lua
@@ -12,15 +12,16 @@ db.custom_header = {
 
 db.center_pad = 5
 db.custom_center = {
-  {icon = "", desc = "Open Telescope Fined Files           ", shortcut = "", action = "Telescope find_files"},
-  {icon = "", desc = "Open Telescope File Browser          ", shortcut = "", action = "Telescope file_browser"},
-  {icon = "", desc = "dashboardRecently lastest session    ", shortcut = "", action = "SessionLoad"},
-  {icon = "", desc = "Recently opened files                ", shortcut = "", action = "DashboardFindHistory"},
-  {icon = "", desc = "Open Personal dotfiles               ", shortcut = "", action = "edit $MYVIMRC"}
+  {icon = "", desc = "Open Telescope Fined Files  ", shortcut = "", action = "Telescope find_files"},
+  {icon = "", desc = "Open Personal dotfiles      ", shortcut = "", action = "edit $MYVIMRC"},
+  {icon = "", desc = "Install Plugs               ", shortcut = "", action = "PlugInstall"},
+  {icon = "", desc = "Update  Plugs               ", shortcut = "", action = "PlugUpdate"},
+  {icon = "", desc = "Clear   Plugs               ", shortcut = "", action = "PlugClean"},
+  
 }
 
 db.footer_pad = 5
-db.custom_footer = {"Stopping your Mouse!", "Using your Keyboard!"}
+db.custom_footer = {"You seen, you standing there!"}
 
 -- db.session_directory = "~/.config/nvim/.session"
 -- db.session_auto_save_on_exit = true
