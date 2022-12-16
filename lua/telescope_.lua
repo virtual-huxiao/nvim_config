a844417c6524ac9c5b863558025fbde99501ee8f
胡箫儿 <345609501@qq.com>
2022-12-16 22:25:55 +0800

    open telescope.live_grep, and change the telescope theme with different mode

--- lua/telescope_.lua
+++ lua/telescope_.lua
@@ -8,6 +8,8 @@ end
 ------------------------------
 require("telescope").setup {
   defaults = {
+    layout_strategy = 'vertical',
+    layout_config = { height = 0.95 },
     mappings = {
       n = {
         ["q"] = actions.close,
@@ -23,16 +25,26 @@ require("telescope").setup {
       override_file_sorter = true, -- override the file sorter
       case_mode = "smart_case" -- or "ignore_case" or "respect_case"
       -- the default case_mode is "smart_case"
+    },
+  },
+  pickers = {
+    find_files = {
+      theme = "dropdown",
+    },
+    buffers = {
+      theme = "ivy",
+    },
+    marks = {
+      theme = "cursor",
     }
   }
 }
-
 
 --按键设置
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 -- vim.keymap.set('n', '<leader>fgf', builtin.git_files, {}) -- 通过git ls-files搜索, 也即项目中的内容(find_files如果没有发现git则显示目录)
--- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {}) -- 依赖于ripgrep:https://github.com/chinanf-boy/ripgrep-zh
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {}) -- 依赖于ripgrep:https://github.com/chinanf-boy/ripgrep-zh
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fm', builtin.marks, {})
